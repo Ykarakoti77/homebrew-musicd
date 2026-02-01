7f3af26ec5cb237ad77d038d73a74c7bdff5abbf
Yash Karakoti <karakotiyash01@gmail.com>
2026-02-01 13:26:08 +0530

    musicd v0.2.0

--- Formula/musicd.rb
+++ Formula/musicd.rb
@@ -3,12 +3,12 @@ class Musicd < Formula
 
   desc "Terminal-native music player daemon powered by mpv and yt-dlp"
   homepage "https://github.com/Ykarakoti77/musicd"
-  url "https://github.com/Ykarakoti77/musicd/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  url "https://github.com/Ykarakoti77/musicd/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "1cf847421644201ebc54a27be7c731ce6739b7543b38510787fa68cbb4a3320c"
   license "MIT"
 
-  depends_on "python@3.12"
   depends_on "mpv"
+  depends_on "python@3.12"
   depends_on "yt-dlp"
 
   def install
